4a766d5533ac792a161bf4ddd582e426e12dce46
VULTRON <csbabcock@protonmail.com>
2026-09-20 08:00:48 -0400

    testing basic functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+
+fn add_kill(kills: i32) -> i32 {
+    kills + 1
 }
+
+fn main() {
+    let kills = 0;
+    let updated_kills = add_kill(kills);
+
+    println!("Kills: {updated_kills}");
+}
+
